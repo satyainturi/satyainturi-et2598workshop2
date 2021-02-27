a21f2d6eeae9789dfdab1f21d6b125ad358ab4fe
satyainturi <satyainturi07@gmail.com>
2021-02-27 15:41:28 +0100

    add ruditest file

---
+++ ruditest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+k=1
+while [ $k -le 3 ]
+do
+    {% for host in groups['haproxy'] %}
+    curl  {{ hostvars[host]['ansible_default_ipv4']['address']}}
+    {% endfor %}
+    k=$(($k+1))
+done
